a4d000785cb35ea8583048d2a7138e6216aefe9e
mankai-amine <medaminemankai@gmail.com>
2024-03-21 14:44:58 -0400

    isSpecialChar()

--- Validator/src/Validator.java
+++ Validator/src/Validator.java
@@ -3,13 +3,22 @@ public class Validator {
 
 	public static void main(String[] args) {
 		System.out.println(isAlphaNum('7'));
-		
+		System.out.println(isSpecialChar('-', false));
 	}
 	
+	// isAlphaNum()
 	public static boolean isAlphaNum(char character) {
 		return Character.isLetterOrDigit(character);		
 	}
 
-	
+	// isSpecialChar()
+	public static boolean isSpecialChar (char character, boolean bool) {
+		if (character == '-' || character == '.') {
+			return true;
+		} else if  (character == '_' && bool){
+			return true;
+		} else 
+			return false;
+	}
 	
 }
