c2048fd4eb784eee39a0ff49e7a9066008264511
iasssy <161367934+iasssy@users.noreply.github.com>
2024-03-23 10:42:13 -0400

    added new methods

--- Validator/src/Validator.java
+++ Validator/src/Validator.java
@@ -11,7 +11,8 @@ public class Validator {
 		System.out.println(singleAtSign("username@domain.com"));
 		System.out.println(fetchBeforeAt("username@domain.com"));
 		System.out.println(fetchAfterAt("username@domain.com"));
-		System.out.println(isPrefix("you_me"));
+		System.out.println(isPrefix("you_!9850348me"));
+		System.out.println(isDomain("ac-133.yul"));
 	}
 	
 	// isAlphaNum() to check if a character is alphanumeric
@@ -52,45 +53,91 @@ public class Validator {
 	//fetchBeforeAt() to get the beginning of an email address
 	public static String fetchBeforeAt(String email) {
 		String temp="";
-		for ( int i = 0; i < email.length(); i++) {
+		int i=0;
+		for (i = 0; i < email.length(); i++) {
 			if (email.charAt(i)=='@') {
-				temp=email.substring(0, i);
+				break;
 			}			
 		}
-		return temp;		
+		return email.substring(0, i);	
 	}
 	
 	//fetchAfterAt() to get the ending of an email address
 	public static String fetchAfterAt(String email){
-		String temp="";
-		for ( int i = 0; i < email.length(); i++) {
+		int i=0;
+		for (i = 0; i < email.length(); i++) {
 			if (email.charAt(i)=='@') {
-				temp=email.substring(i+1, email.length());
+				break;
 			}			
 		}
-		return temp;	
+		return email.substring(i+1, email.length());	
 	}
 	
 	//isPrefix() to check if the start of a String is a valid email prefix
 	public static boolean isPrefix(String prefix) {
-		if (prefix.length()==0 || !isAlphaNum(prefix.charAt(0))) {
+		if (prefix.length()==0 || !isAlphaNum(prefix.charAt(0)) || isAlphaNum(prefix.charAt(prefix.length()-1))) {
 	        return false; // Empty prefix or first character is not alphanumeric
 	    }
 		
-	    for (int i=0; i<prefix.length(); i++) {
+	    for (int i=1; i<prefix.length()-1; i++) {
 	    	 if (!isPrefixChar(prefix.charAt(i))) {
 	             return false; // Invalid character
 	         }
 	         
 	         if (isSpecialChar(prefix.charAt(i), true)) {
-	             if ((i + 1)>=prefix.length() || !isAlphaNum(prefix.charAt(i + 1))) {
+	             if (!isNextCharacter(i, prefix)) {
 	                 return false; // Special character not followed by alphanumeric character
 	             }
 	         }
-	    }
-	    
+	    }	    
 	    return true; // All characters passed validation		
 	}
+	
+	public static boolean isNextCharacter(int i, String str) {
+		return ((i + 1)<str.length() && isAlphaNum(str.charAt(i + 1)));
+	}
+	
+	public static boolean isDomain(String domain) {	
+		if (domain.length() < 3) {
+			//domain should have at least two portions separated by a period, that is 3 characters
+			return false; 
+		}
+		int dot=domain.indexOf('.');//index of period character in a string domain 
+		if (dot<=0 || dot==domain.length()-1) {
+			// indexOf returns -1 when the character is not found
+			// if indexOf return 0, the first character in string is "."
+			return false;
+		}
+		String firstPortion=domain.substring(0,dot);
+		String secondPortion=domain.substring(dot+1);
+		
+		//check the first portion	
+		if (!isAlphaNum(firstPortion.charAt(0))){
+			return false; // First portion must start with an alphanumeric character
+		}
+		
+		for (int i = 0; i < firstPortion.length(); i++) {
+	        char currentChar = firstPortion.charAt(i);
+	        if (!isDomainChar(currentChar)) {
+	            return false; // Invalid character in the first portion
+	        }
+
+	        if (isSpecialChar(currentChar, true)) {
+	            if (isNextCharacter(i,firstPortion)) {
+	                return false; // Period or dash in the first portion must be followed by one or more alphanumeric characters
+	            }
+	        }
+	    }
+
+	    // Check second portion
+	    if (secondPortion.length() < 2 || !secondPortion.chars().allMatch(Character::isLetter)) {
+	        return false; // Second portion must have at least two characters and contain only letters of the alphabet
+	    }
+
+		
+		return true;
+	}
+	
 	
 	
 }
